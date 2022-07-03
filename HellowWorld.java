e79048b068061ed9163a52129c7e7886529bf26d
krish <krish@gmail.com>
2022-07-03 23:01:06 +0530

    first commit

---
+++ HellowWorld.java
@@ -0,0 +1,9 @@
+class HellowWorld{
+public static void main(String[] args){
+System.out.println("Hello......Kishan ......!");
+System.out.println("Hello......HYDERABAD ......!");
+System.out.println("Hello..How are You......!");
+System.out.println("Hello..How are You......1");
+System.out.println("Hello..How are You......2");
+}
+}
